01f48e708a31c0834f94b8fa40a98b04ed799f77
marcosaurelio36 <marcos.aureliodepadua@gmail.com>
2023-06-07 10:12:31 -0300

    Foi adicionado uma condição ao valor digitado

--- scripts.js
+++ scripts.js
@@ -4,9 +4,15 @@ function drawANumber () {
     const max = Math.ceil ( document.querySelector(".input-max").value) 
     const min = Math.floor ( document.querySelector(".input-min").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1) + min);
+    if (min >= max){
+        alert(" 🚨 O valor minino deve ser MENOR que o valor maximo🚨")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1) + min);
+    
+
+ 
     alert (result)
-      
+    }
 
 
 }
